eac1b43bff7dfbd86fd2a4963fdf259e68fcb392
stepanov96-96 <stepanov96_96@mail.ru>
2022-05-24 19:38:36 +0300

    Styless

--- App.js
+++ App.js
@@ -1,14 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import React from 'react';
+import { StyleSheet, Text, View, Button, Alert } from 'react-native';
 
 export default function App() {
   const handleTextPress = () => console.log('Text pressed'); 
-  const handlebuttonText = () => alert('jopa');
+  const handlebuttonText = () => Alert.alert("Title text", "main title",[
+    {text: "Да", onPress: () => console.log('yes button')},
+    {text: "Отмена" , onPress: () => console.log('no button')}
+  ]);
+  const handlebuttonText2 = () => Alert.prompt("Title text", "main title",text => console.log );
 
   return (
     <View style={styles.container}>
-      <Text style = {styles.text} onPress = {handleTextPress} > Макс </Text>
-      <Button title = {'syka vsd'} onPress = {handlebuttonText}/>
+        <View style = {styles.box}>
+          <Text style = {styles.textBox}> Click me</Text>
+        </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -24,4 +30,22 @@ const styles = StyleSheet.create({
   text: {
     color: 'red',
   },
+
+  box: {
+    backgroundColor: '#FF6347',
+    width: '50%',
+    height: '10%',
+    borderWidth: 2,
+    borderColor: 'black',
+    position: "absolute",
+    top: '50%',
+    
+  },
+
+  textBox:{
+    fontSize: 20,
+    position: "absolute",
+    top: '25%',
+    left: '25%'
+  }
 });
